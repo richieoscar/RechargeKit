5ae7806046f7c6af0910f427bfcdcbd43ced9741
Oscar Anyiam <oscaranyiam94@gmail.com>
2020-09-30 19:49:57 +0100

    Added phone dialer intent

--- app/src/main/java/com/example/rechargekit/DetailsActivity.java
+++ app/src/main/java/com/example/rechargekit/DetailsActivity.java
@@ -1,20 +1,25 @@
 package com.example.rechargekit;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class DetailsActivity extends AppCompatActivity {
 
     private Button done;
-    private EditText phoneNumber;
+    private EditText network;
     private EditText rechargePin;
     private EditText amount;
+    private Button cardRecharge;
+    private Button bankRecharge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,16 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         amount = findViewById(R.id.editText_amount);
         rechargePin = findViewById(R.id.editText__recharge_pin);
-        phoneNumber = findViewById(R.id.editText_phone_number);
+        network = findViewById(R.id.editText_network);
         done = findViewById(R.id.button_done);
+        cardRecharge = findViewById(R.id.button_card_recharge);
+        bankRecharge = findViewById(R.id.button_bank_recharge);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Airtime Recharge");
 
-        getData();
+
+        rechargeCArd();
+        bankRecharge();
     }
 
     @Override
@@ -36,27 +47,135 @@ public class DetailsActivity extends AppCompatActivity {
 
     private void getData(){
         done.setOnClickListener((view)->{
-            String codeEnd = "#";
-          String airtimeAmount=  amount.getText().toString();
            String airtimeRechargePin = rechargePin.getText().toString();
+            final String airtel_id ="airtel";
+            final  String mtn_id = "mtn";
+            final String glo_id = "glo";
+            final String nine_mobile = "9mobile";
 
 
-
-           if(TextUtils.isEmpty(amount.getText())){
-               amount.setError("Field is Empty");
-               return;
-           }
            if(TextUtils.isEmpty(rechargePin.getText())){
                rechargePin.setError("Field is Empty");
                return;
            }
+           if (TextUtils.isEmpty(network.getText())){
+               network.setError("Field is Empty");
+               return;
+           }
 
-         //  String code = RechargeCodes.getAirtelRechargeCode() +airtimeRechargePin +codeEnd;
-           String code = String.format("%s %s %s",RechargeCodes.getAirtelRechargeCode(), airtimeRechargePin, codeEnd);
+           if(network.getText().toString().toLowerCase().equals(airtel_id)){
+               String code = String.format("%s %s %s",RechargeCodes.getAirtelRechargeCode(), airtimeRechargePin, RechargeCodes.getCodeEnd());
+               Uri uri = Uri.parse("tel:" +code );
+               Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
+               startActivity(dialer);
+                return;
+           }  else Toast.makeText(this, "Network not Recognized",Toast.LENGTH_SHORT).show();
 
-            Uri uri = Uri.parse("tel:" +code );
-           Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
-           startActivity(dialer);
+            if(network.getText().toString().toLowerCase().equals(mtn_id)){
+                String code = String.format("%s %s %s",RechargeCodes.getMtnRechargeCode(), airtimeRechargePin, RechargeCodes.getCodeEnd());
+                Uri uri = Uri.parse("tel:" +code );
+                Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(dialer);
+                return;
+            } else Toast.makeText(this, "Network not Recognized",Toast.LENGTH_SHORT).show();
+
+            if(network.getText().toString().toLowerCase().equals(glo_id)){
+                String code = String.format("%s %s %s",RechargeCodes.getGloRechargeCode(), airtimeRechargePin, RechargeCodes.getCodeEnd());
+                Uri uri = Uri.parse("tel:" +code );
+                Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(dialer);
+                return;
+
+            } else Toast.makeText(this, "Network not Recognized",Toast.LENGTH_SHORT).show();
+
+            if(network.getText().toString().toLowerCase().equals(nine_mobile)){
+                String code = String.format("%s %s %s",RechargeCodes.getNineMobileRechargeCode(), airtimeRechargePin, RechargeCodes.getCodeEnd());
+                Uri uri = Uri.parse("tel:" +code );
+                Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(dialer);
+                return;
+            }
+            else Toast.makeText(this, "Network name not recognized", Toast.LENGTH_SHORT).show();
+
+        });
+
+    }
+
+    private void  rechargeCArd(){
+        cardRecharge.setOnClickListener((view)->{
+            rechargePin.setVisibility(View.VISIBLE);
+            network.setVisibility(View.VISIBLE);
+            done.setVisibility(View.VISIBLE);
+            getData();
+        });
+
+    }
+    private  void bankRecharge(){
+       bankRecharge.setOnClickListener((view)->{
+           rechargePin.setVisibility(View.VISIBLE);
+           rechargePin.setHint("Enter Amount");
+           network.setVisibility(View.VISIBLE);
+           network.setHint("Enter Bank");
+           done.setVisibility(View.VISIBLE);
+           getDataForBankRecharge();
+       });
+    }
+    private void getDataForBankRecharge(){
+        done.setOnClickListener((view)->{
+            String airtimeRechargePin = rechargePin.getText().toString(); //this editText serves for both airtime recharge pin and amount for bank recharge
+            final String GTBANK_ID ="gtbank";
+            final  String FB_ID = "firstbank";
+            final String ZENITH_ID = "zenith";
+            final String UBA_ID  = "uba";
+
+
+            if(TextUtils.isEmpty(rechargePin.getText())){
+                rechargePin.setError("Field is Empty");
+                return;
+            }
+            if (TextUtils.isEmpty(network.getText())){
+                network.setError("Field is Empty");
+                return;
+            }
+
+            if(network.getText().toString().toLowerCase().equals(GTBANK_ID)){
+                String code = String.format("%s %s %s",RechargeCodes.getGtbCode(), airtimeRechargePin, RechargeCodes.getCodeEnd());
+                Uri uri = Uri.parse("tel:" +code );
+                Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(dialer);
+                return;
+            }  else Toast.makeText(this, "Bank name not Recognized",Toast.LENGTH_SHORT).show();
+
+            if(network.getText().toString().toLowerCase().equals(FB_ID)){
+                String code = String.format("%s %s %s",RechargeCodes.getFbCode(), airtimeRechargePin, RechargeCodes.getCodeEnd());
+                Uri uri = Uri.parse("tel:" +code );
+                Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(dialer);
+                return;
+            } else Toast.makeText(this, "Bank name not Recognized",Toast.LENGTH_SHORT).show();
+
+            if(network.getText().toString().toLowerCase().equals(ZENITH_ID)){
+                String code = String.format("%s %s %s",RechargeCodes.getZenithCode(), airtimeRechargePin, RechargeCodes.getCodeEnd());
+                Uri uri = Uri.parse("tel:" +code );
+                Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(dialer);
+                return;
+
+            } else Toast.makeText(this, "Bank name not Recognized",Toast.LENGTH_SHORT).show();
+
+            if(network.getText().toString().toLowerCase().equals(UBA_ID)){
+                String code = String.format("%s %s %s",RechargeCodes.getUbaCode(), airtimeRechargePin, RechargeCodes.getCodeEnd());
+                Uri uri = Uri.parse("tel:" +code );
+                Intent dialer = new Intent(Intent.ACTION_DIAL,uri);
+                startActivity(dialer);
+                return;
+            }
+            else Toast.makeText(this, "Bank name not recognized", Toast.LENGTH_SHORT).show();
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/rechargekit/RechargeCodes.java
+++ app/src/main/java/com/example/rechargekit/RechargeCodes.java
@@ -3,25 +3,77 @@ package com.example.rechargekit;
 import androidx.annotation.NonNull;
 
 public class RechargeCodes {
-    private static  final String mtnRechargeCode = "*555*" ;
-    private  static  final  String airtelRechargeCode = "*126*" ;
-    private  static  final  String gloRechargeCode = "*123*" +"#";
-    private  static  final  String nineMobileRechargeCode = "*222*";
+    private static  final String MTN_RECHARGE_CODE = "*555*" ;
+    private  static  final  String AIRTEL_RECHARGE_CODE = "*126*" ;
+    private  static  final  String GLO_RECHARGE_CODE = "*123*" +"#";
+    private  static  final  String NINE_MOBILE_RECHARGE_CODE = "*222*";
+    private static  final  String CODE_END = "#";
+    private  static  final String GTB_CODE = "*737*";
+    private  static  final String FB_CODE = "*894*";
+    private  static  final String UBA_CODE = "*919*";
+    private  static  final String ZENITH_CODE = "*966*";
+    private  static  final String UB_CODE = "*826*";
+    private  static  final String FIDELITY_CODE = "*770*";
+    private  static  final String ECO_BANK_CODE = "*326*";
+    private  static  final String POLARIS_CODE = "*833*";
+
+    public static String getGtbCode() {
+        return GTB_CODE;
+    }
+
+    public static String getFbCode() {
+        return FB_CODE;
+    }
+
+    public static String getUbaCode() {
+        return UBA_CODE;
+    }
+
+    public static String getZenithCode() {
+        return ZENITH_CODE;
+    }
+
+    public static String getUbCode() {
+        return UB_CODE;
+    }
+
+    public static String getFidelityCode() {
+        return FIDELITY_CODE;
+    }
+
+    public static String getEcoBankCode() {
+        return ECO_BANK_CODE;
+    }
+
+    public static String getPolarisCode() {
+        return POLARIS_CODE;
+    }
+
+    public static String getStanBicCode() {
+        return STAN_BIC_CODE;
+    }
+
+    private  static  final String STAN_BIC_CODE = "*909*";
+
+
+    public static String getCodeEnd() {
+        return CODE_END;
+    }
 
     public static String getMtnRechargeCode() {
-        return mtnRechargeCode;
+        return MTN_RECHARGE_CODE;
     }
 
     public static String getAirtelRechargeCode() {
-        return airtelRechargeCode;
+        return AIRTEL_RECHARGE_CODE;
     }
 
     public static String getGloRechargeCode() {
-        return gloRechargeCode;
+        return GLO_RECHARGE_CODE;
     }
 
     public static String getNineMobileRechargeCode() {
-        return nineMobileRechargeCode;
+        return NINE_MOBILE_RECHARGE_CODE;
     }
 
     @NonNull
